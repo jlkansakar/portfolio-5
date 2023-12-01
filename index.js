6233551d6b76c365226bdeac53aa7b2c45c1a17a
jlkansakar <joka0002@stud.kea.dk>
2023-12-01 05:32:52 +0100

    made all lists :^)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = 3001;
-const mysql = require("mysql2");
 
+
+// The info might have to be changed to work when checking code @benjamin/@nicklas - unsure.
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -18,24 +20,64 @@ connection.connect((error) => {
     }
 });
 
-
-app.get("/list", (req, res) => {
-    res.send("Hello World!")
-
+// list of all games
+connection.query("select * from games", function (error, result) {
+    if (error) throw error;
+    app.get("/gamelist", (req, res) => {
+        res.send(result)
+    })
 });
 
-connection.connect(function(error) {
+// list of all players
+connection.query("select * from players", function (error, result) {
     if (error) throw error;
-    console.log("Connected");
-    connection.query("select * From players inner join games on players.gameID = games.gameID", function (error, result) {
-        if (error) throw error;
-        app.get("/biglist", (req, res) => {
-            res.send(result)
-        })
+    app.get("/playerlist", (req, res) => {
+        res.send(result)
+    })
+});
+
+// list for Celeste players
+connection.query("select * from players inner join games on players.gameID = games.gameID where games.gameID = 1", function (error, result) {
+    if (error) throw error;
+    app.get("/playerlist/celeste", (req, res) => {
+        res.send(result)
+    })
+});
+
+// list for Ultrakill players
+connection.query("select * from players inner join games on players.gameID = games.gameID where games.gameID = 2", function (error, result) {
+    if (error) throw error;
+    app.get("/playerlist/ultrakill", (req, res) => {
+        res.send(result)
+    })
+});
+
+// list for Super Mario 64 players
+connection.query("select * from players inner join games on players.gameID = games.gameID where games.gameID = 3", function (error, result) {
+    if (error) throw error;
+    app.get("/playerlist/supermario64", (req, res) => {
+        res.send(result)
+    })
+});
+
+// list for Hitman Trilogy players
+connection.query("select * from players inner join games on players.gameID = games.gameID where games.gameID = 4", function (error, result) {
+    if (error) throw error;
+    app.get("/playerlist/hitmantrilogy", (req, res) => {
+        res.send(result)
+    })
+});
+
+// full list of both players and games combined into one table.
+
+connection.query("select * From players inner join games on players.gameID = games.gameID", function (error, result) {
+    if (error) throw error;
+    app.get("/fulllist", (req, res) => {
+        res.send(result)
     })
 });
 
 app.listen(port, () => {
     console.log(`speedrun is listening on port ${port}`)
 });
-// commit force
+
